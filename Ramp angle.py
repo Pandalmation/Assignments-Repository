92abbb6808895c1cc5e10d94f68831093820d1a7
Pandalmation <91600281+Pandalmation@users.noreply.github.com>
2021-11-09 12:34:43 +0700

    deleteee

--- Ramp angle.py
+++
@@ -1,8 +0,0 @@
-import math
-def angle(m,g,f):
-        return math.degrees(math.asin(f/(m*g)))
-result=0
-g= 9.8;"m/s**2"
-m=float(input("Enter the mass of the cart (in kg): "))
-f=float(input("Enter the force to push the cart (in N): "))
-if m>0 or m==0 : print("The angle of the ramp is","{:.1f} degrees".format(angle(m,g,f)))
